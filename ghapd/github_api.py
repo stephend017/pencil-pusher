698678f971987898f4818ed6f556ecad528f6764
Stephen Davis <davi1138@purdue.edu>
2021-03-09 11:31:49 -0500

    Added basic git functionality

---
+++ ghapd/github_api.py
@@ -0,0 +1,120 @@
+"""
+Wrapper class for managing complex github commands needed to be
+executed for this action
+"""
+import subprocess
+from github import Github
+from github.ContentFile import ContentFile
+from github.Repository import Repository
+import logging
+import os
+
+
+logger = logging.getLogger("ghapi")
+logger.addHandler(logging.FileHandler("ghapi.log"))
+logger.setLevel(logging.INFO)
+
+
+class GithubAPI:
+    def __init__(self, personal_access_token: str = ""):
+        """
+        """
+        self._has_token = personal_access_token != ""
+        self._pat = personal_access_token
+        if self._has_token:
+            self._Github = Github(personal_access_token)
+        else:
+            self._Github = Github()
+
+    def clone_repo(self, owner: str, repo: str, local_path: str = ""):
+        """
+        """
+        cwd = os.path.abspath(
+            os.path.join(
+                os.path.dirname(os.path.abspath("./setup.py")), local_path
+            )
+        )
+
+        url = f"https://github.com/{owner}/{repo}.git"
+        if self._has_token:
+            url = f"https://{owner}:{self._pat}@github.com/{owner}/{repo}.git"
+
+        subprocess.Popen(
+            ["git", "clone", f"{url}", f"{local_path}"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        subprocess.Popen(
+            [
+                "git",
+                "remote",
+                "set-url",
+                "origin",
+                f"https://{owner}:{self._pat}@github.com/{owner}/{repo}.git",
+            ],
+            cwd=cwd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        subprocess.Popen(
+            ["git", "remote", "-v"],
+            cwd=cwd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+    def full_update(self, path: str):
+        """
+        stages, commits and pushes all changes to a git repo
+        in the given directory
+        """
+        subprocess.Popen(
+            ["git", "config", "user.name", "ghapd"],
+            cwd=path,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        subprocess.Popen(
+            ["git", "config", "user.email", "<>"],
+            cwd=path,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        subprocess.Popen(
+            ["git", "add", "."],
+            cwd=path,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        subprocess.Popen(
+            ["git", "commit", "-m", '"Auto commit by ghapd"'],
+            cwd=path,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        subprocess.Popen(
+            ["git", "push"],
+            cwd=path,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+    @staticmethod
+    def get_public_file(owner: str, repo: str, file_path: str) -> str:
+        """
+        Gets a file from a public repo
+        """
+        g = Github()
+        repo: Repository = g.get_repo(f"{owner}/{repo}")
+        response: ContentFile = repo.get_contents(file_path)
+        return str(response.decoded_content)
+
+    def _require_token(self):
+        """
+        """
+        if not self._has_token:
+            raise ValueError(
+                "The selected operation requires that a token be provided"
+            )
---
+++ tests/test_github_api.py
@@ -0,0 +1,82 @@
+from ghapd.github_api import GithubAPI
+import os
+import time
+import shutil
+
+REPO = "ghapd"
+OWNER = "stephend017"
+ROOT_DIR = os.path.dirname(os.path.abspath("./setup.py"))
+
+
+def test_get_public_file():
+    """
+    Tests that a public file can be successfully queried from
+    a public repository
+    """
+    file_path = "action.yml"
+
+    contents = GithubAPI.get_public_file(OWNER, REPO, file_path)
+
+    assert 'name: "ghapd"' in contents
+
+
+def test_clone_repo():
+    """
+    Tests that a repo can be cloned successfully to a
+    local location
+    """
+    ghapi = GithubAPI()
+    ghapi.clone_repo(OWNER, REPO, "example-repo")
+    path = os.path.join(ROOT_DIR, "example-repo")
+
+    # python is slow and will not recognize the new directory right away
+    time.sleep(1)
+
+    print(path)
+    assert os.path.exists(path)
+    shutil.rmtree(path)
+    time.sleep(1)
+
+
+def test_clone_repo_external():
+    """
+    Tests that a repo can be cloned successfully to a
+    local location that is outside of the executing directory
+
+    This is needed so the wiki repo and the source repo can be
+    used in tandem without having to deal with git potentially
+    interacting with both of them
+    """
+    ghapi = GithubAPI()
+    ghapi.clone_repo(OWNER, REPO, "../example-repo")
+    path = os.path.join(ROOT_DIR, "../example-repo")
+
+    # python is slow and will not recognize the new directory right away
+    time.sleep(1)
+
+    assert os.path.exists(path)
+    shutil.rmtree(path)
+    time.sleep(1)
+
+
+def test_full_send():
+    """
+    Tests that a repo can be automatically staged, committed and
+    pushed
+    """
+    ghapi = GithubAPI(os.environ["GH_PAT"])
+    ghapi.clone_repo(OWNER, "ghapd.wiki", "../example-repo")
+    path = os.path.join(ROOT_DIR, "../example-repo")
+
+    # python is slow and will not recognize the new directory right away
+    time.sleep(1)
+
+    # do a change
+    with open(os.path.join(path, "Home.md"), "a+") as fp:
+        fp.write("* testing string \n\n")
+
+    ghapi.full_update(path)
+
+    time.sleep(1)
+    shutil.rmtree(path)
+    time.sleep(1)
