ab06e0f599d9932361f41e5923c1adbbc4a4dc7b
stephend017 <stephenedavis17@gmail.com>
2021-03-10 17:56:16 -0500

    Most basic main file implemented

--- ghapd/__main__.py
+++ ghapd/__main__.py
@@ -1,12 +1,40 @@
-import os
+import time
+from ghapd.documenter import Documenter
+from ghapd.input import InputManager
+from ghapd.github_api import GithubAPI
+from ghapd.repo_manager import RepoManager
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    im = InputManager()
+    im.define()
+    response = im.validate()
 
-    my_output = f"Hello {my_input}"
+    if not response[0]:
+        # TODO print error (response[1]) idk
+        exit(1)
 
-    print(f"::set-output name=myOutput::{my_output}")
+    Documenter.install()
+    time.sleep(1)
+
+    gh = GithubAPI(im.get("personal_access_token"))
+
+    rm = RepoManager(im.get("owner_name"), im.get("repository_name"), gh)
+
+    rm.setup()
+    time.sleep(1)
+
+    rm.install()
+    time.sleep(1)
+
+    # only support default for now (repo_name = package_name)
+    rm.document()
+    time.sleep(1)
+
+    rm.publish()
+    time.sleep(1)
+
+    rm.cleanup()
 
 
 if __name__ == "__main__":
