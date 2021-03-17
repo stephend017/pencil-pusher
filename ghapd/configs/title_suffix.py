eba6475e09b92efeca6666adcaa794d04b65be13
Stephen Davis <davi1138@purdue.edu>
2021-03-16 21:41:50 -0400

    Title prefix & suffix (#27)
    * Implemented title prefix
    
    * Added title suffix

---
+++ ghapd/configs/title_suffix.py
@@ -0,0 +1,37 @@
+from ghapd.configs.manager import ConfigManager
+from typing import Any
+from ghapd.configs.base import ConfigParamBase
+from ghapd.configs import config_manager
+
+
+@config_manager.register("title_suffix")
+class TitleSuffixConfigParam(ConfigParamBase):
+    """
+    Processes the title_suffix config parameter
+
+    Note: validation only validates config name
+        and config type (string array)
+    """
+
+    def on_iterate(self, data: Any):
+        """
+        This is the validate function
+        """
+        if data["type"] == ConfigManager.VALIDATE:
+            self._validate(data["contents"])
+            self.set_defined()
+
+    def _validate(self, contents: dict) -> bool:
+        """
+        Validates the parameter is included in the
+        config file since it is a required field
+        """
+        if "title_suffix" not in contents:
+            raise ValueError("title_suffix config parameter was not defined")
+
+        if not isinstance(contents["title_suffix"], str):
+            raise ValueError(
+                "title_suffix config parameter is not of type str"
+            )
+
+        return True
---
+++ tests/test_configs/test_title_suffix.py
@@ -0,0 +1,41 @@
+from tests.test_configs.config_helper import (
+    load_and_validate,
+    load_and_validate_fails,
+    load_config,
+)
+from ghapd.configs import config_manager
+
+
+def test_valid():
+    """
+    Tests that a valid config file contains the required
+    parameter title_suffix
+    """
+    load_config()
+    load_and_validate()
+
+
+def test_invalid_type():
+    """
+    """
+    load_config(title_suffix=3)
+    load_and_validate_fails()
+
+
+def test_not_defined():
+    """
+    """
+    load_config(exclude=["title_suffix"])
+    load_and_validate_fails()
+
+
+def test_get_title_prefix():
+    """
+    """
+    expected = "mysuffix"
+
+    load_config(title_suffix=expected)
+    load_and_validate()
+
+    response = config_manager.get("title_suffix")
+    assert response == expected
