5aeba211a37aa96a6855059019981344f12c98aa
Stephen Davis <davi1138@purdue.edu>
2021-03-19 09:01:35 -0400

    New name (#39)
    * renamed package to pencil_pusher
    
    * v0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,16 @@ setup(
     version="0.0.2",
     description="A python package that compiles source documentation and publishes it to the repo wiki",
     long_description=readme,
+    long_description_content_type="text/markdown",
     author="Stephen Davis",
     author_email="stephenedavis17@gmail.com",
     packages=find_packages(),
+    # TODO rename this later
+    url="https://github.com/stephend017/ghapd",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
