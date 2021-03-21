0fd9983d574c57343dea3af4cd89b86f00a53f35
stephend017 <stephenedavis17@gmail.com>
2021-03-21 11:32:35 -0400

    v1 final version

--- pencil_pusher/__main__.py
+++ pencil_pusher/__main__.py
@@ -15,7 +15,7 @@ def main():
 
     Documenter.install()
 
-    gh = GithubAPI(im.get("personal_access_token"))
+    gh = GithubAPI(im.get("github_token"))
 
     owner_name = im.get("owner_name")
     repository_name = im.get("repository_name")
@@ -29,7 +29,11 @@ def main():
     config_manager.validate()
 
     langs = config_manager.get_langs()
-    extensions = [ext for ext in [ext_list for _, ext_list in langs.items()]]
+    # extensions = [ext for ext in [ext_list for _, ext_list in langs.items()]]
+    extensions = []
+    for _, ext_list in langs.items():
+        for ext in ext_list:
+            extensions.append(ext)
 
     # transform titles config into usable dict
     titles = {}
