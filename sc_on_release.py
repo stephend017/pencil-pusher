140667b9cba2e63fed2b431cf0d526160cefde82
Stephen Davis <davi1138@purdue.edu>
2021-04-16 16:17:04 -0400

    Update sc_on_release.py

--- sc_on_release.py
+++ sc_on_release.py
@@ -13,8 +13,8 @@ dockerfile_sha = dockerfile_meta.sha
 setup_file = str(repo.get_contents("setup.py").decoded_content, 'utf-8')
 
 
-old_version = re.search(r"\\d+\\.\\d+\\.\\d+", dockerfile).group()
-new_version = re.search(r'\\d+\\.\\d+\\.\\d+', setup_file).group()
+old_version = re.search(r"\d+\.\d+\.\d+", dockerfile).group()
+new_version = re.search(r'\d+\.\d+\.\d+', setup_file).group()
 
 dockerfile = dockerfile.replace(old_version, new_version)
 repo.update_file("Dockerfile", "Updated version", dockerfile, dockerfile_sha)
