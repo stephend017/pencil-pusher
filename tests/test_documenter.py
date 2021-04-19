7fa0efe4b71347741f93f5b54f1b201832019a9f
stephend017 <stephenedavis17@gmail.com>
2021-04-19 09:59:55 -0400

    Added basic sidebar support

--- tests/test_documenter.py
+++ tests/test_documenter.py
@@ -10,3 +10,35 @@ def test_gen_local():
     Documenter.generate("pencil_pusher.documenter", "test")
     assert os.path.isfile("./test.md")
     os.remove("./test.md")
+
+
+def test_toc():
+    """
+    Tests that toc is generated correctly
+    """
+
+    test_files = [
+        "pencil_pusher.configs.base.py",
+        "pencil_pusher.configs.manager.py",
+        "pencil_pusher.configs.python_lang.py",
+        "pencil_pusher.documenter.py",
+        "pencil_pusher.input.py",
+    ]
+
+    test_file_map = {
+        ".".join(i.split(".")[:-1]): ".".join(i.split(".")[:-1])
+        for i in test_files
+    }
+    toc_tree = Documenter.build_toc(test_files)
+
+    assert toc_tree == {
+        "pencil_pusher": {
+            "configs": {"base": {}, "manager": {}, "python_lang": {}},
+            "documenter": {},
+            "input": {},
+        }
+    }
+
+    Documenter.generate_sidebar(toc_tree, test_file_map)
+
+    os.remove("./_Sidebar.md")
